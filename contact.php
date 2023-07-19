58be7fc38d454a911f42aa13fd6bb0345a816075
M-Ammar1112 <ammarshahid877@gmail.com>
2023-07-19 13:26:44 +0500

    changes in contact page

--- contact.php
+++ contact.php
@@ -54,25 +54,25 @@
         <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63225.996807740055!2d80.97815907936754!3d7.934196847392783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afb456e05e5a4c9%3A0x72cd1cfea9d4d0a9!2sPolonnaruwa%20Ancient%20City!5e0!3m2!1sen!2slk!4v1659525623039!5m2!1sen!2slk" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <h5>Address</h5>
         <a href="https://goo.gl/maps/jFdoRUnTvq314zJy6" target="_blank" class="d-inline-block text-decoration-none text-dark mb-2">
-          <i class="bi bi-geo-alt-fill"></i> XYZ, Ancient City, Polonnaruwa.
+          <i class="bi bi-geo-alt-fill"></i> PC, Karachi, pakistan.
         </a>
         <h5 class="mt-4">Call us</h5>
-        <a href="tel: +94768799665" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +94 768799665</a>
+        <a href="tel: +94768799665" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +92 3319999629</a>
         <br>
-        <a href="tel: +94768799665" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +94 768799665</a>
+        <a href="tel: +94768799665" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i> +92 3330282003</a>
         <h5 class="mt-4">Email</h5>
-        <a href="mailto: dineshslweb@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-envelope-fill"></i> dineshslweb@gmail.com</a>
+        <a href="mailto: ammarshahid900@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-envelope-fill"></i> ammarshahid900@gmail.com</a>
 
         <h5 class="mt-4">Follow us</h5>
-        <a href="#" class="d-inline-block text-dark fs-5 me-2">
+        <a href="https://www.twitter.com/" class="d-inline-block text-dark fs-5 me-2">
             <i class="bi bi-twitter me-1"></i>
         </a>
         
-        <a href="#" class="d-inline-block text-dark fs-5 me-2">
+        <a href="https://www.facebook.com/" class="d-inline-block text-dark fs-5 me-2">
             <i class="bi bi-facebook me-1"></i>
         </a>
         
-        <a href="#" class="d-inline-block text-dark fs-5">
+        <a href="https://www.instagram.com/" class="d-inline-block text-dark fs-5">
           <i class="bi bi-instagram me-1"></i>
           
         </a>
